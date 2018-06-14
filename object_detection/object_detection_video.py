ec6c1709fbfee75d51ed8fc2c91c4e0a463c679c
maruthibg <maruthibg@gmail.com>
2018-06-14 05:16:32 +0200

    Update object_detection_video.py

--- object_detection/object_detection_video.py
+++ object_detection/object_detection_video.py
@@ -4,28 +4,14 @@ import six.moves.urllib as urllib
 import sys
 import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
-#from matplotlib import pyplot as plt
-from PIL import Image
-
 import cv2
-path = '/opt/videoupload/VID_20180613_124038.mp4'
-cap = cv2.VideoCapture(r'%s' % (path))
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[3]:
+import argparse
 
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
 
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
@@ -50,7 +36,6 @@ for file in tar_file.getmembers():
 
 
 # ## Load a (frozen) Tensorflow model into memory.
-# In[6]:
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -63,75 +48,64 @@ with detection_graph.as_default():
 # ## Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 
-# In[7]:
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
-# ## Helper code
-# In[8]:
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
+if __name__ == '__main__':
+  # construct the argument parser and parse the arguments
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-p", "--path", required=True, help="path to input video")
+  ap.add_argument("-e", "--enable_imshow", required=False, help="Enable Imshow")
+  args = vars(ap.parse_args())
+  path = args["path"]
+  enable_imshow = int(args.get("enable_imshow", '0'))
+  #path = r'C:\Users\govindareddy\Downloads\VID_20180612_165108.mp4'
+  cap = cv2.VideoCapture(r'%s' % (path))
 
-# # Detection
-# In[9]:
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-# PATH_TO_TEST_IMAGES_DIR = 'test_images'
-# TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-# IMAGE_SIZE = (12, 8)
-
-
-# In[10]:
-
-with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
-    #image_np = cv2.imread(r'd:\PROJECTS\maruthi_utils\scanner\images\image5.jpg')
-    while True:
-      ret, image_np = cap.read()
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-      # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
-      # Visualization of the results of a detection.
-      
-      enable_imshow = False
-      detect_barcode = False
-      result = vis_util.visualize_boxes_and_labels_on_image_array(
-          image_np,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=8,
-          display_string=True
-      )
-      if enable_imshow:
-        cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-          cv2.destroyAllWindows()
+  with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+      while True:
+        ret, image_np = cap.read()
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        # Actual detection.
+        (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        # Visualization of the results of a detection.
+        result = vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            enable_imshow=enable_imshow
+        )
+        if enable_imshow:
+          from matplotlib import pyplot as plt
+          plt.figure(figsize=IMAGE_SIZE)
+          plt.imshow(image_np)        
+          cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+          if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        else:
+          print(result)
           break
-      print(result)
-      break
