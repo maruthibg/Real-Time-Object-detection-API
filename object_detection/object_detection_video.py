83fb52679d0764eb3e6fb17ebf76f56ce0a9ec88
maruthibg <maruthibg@gmail.com>
2018-06-14 18:30:33 +0200

    Update object_detection_video.py

--- object_detection/object_detection_video.py
+++ object_detection/object_detection_video.py
@@ -1,66 +1,114 @@
-from sqlalchemy import create_engine  
-from sqlalchemy import Column, String  
-from sqlalchemy.ext.declarative import declarative_base  
-from sqlalchemy.orm import sessionmaker
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import cv2
+import argparse
 
-from utils import Packet
+from utils import label_map_util
+from utils import visualization_utils as vis_util
 
-db_string = "postgres://postgres:Floorcheck@1234@216.10.249.58:5432/floor_inv"
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
 
-db = create_engine(db_string)  
-base = declarative_base()
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
-class Assets(base):  
-    __tablename__ = 'asset'
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
-    assetid = Column(String, primary_key=True)
-    assetname = Column(String)
-    assetpath = Column(String)
-    assetstatus = Column(String)
-    assetidentificationkey = Column(String)
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
 
-Session = sessionmaker(db)  
-session = Session()
+# ## Download Model
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
 
-#base.metadata.create_all(db)
 
-# Create 
-# ledgers = Ledger(assetid='2', assetname="Doctor Strange", assetpath="Scott Derrickson", assetstatus="2016")  
-# session.add(ledgers)  
-# session.commit()
+# ## Load a (frozen) Tensorflow model into memory.
 
-def get_assets(status=False):
-    results = []
-    rows = session.query(Assets)
-    if status:
-        rows = rows.filter(Assets.assetstatus == status)
-    for row in rows:
-        p = Packet()
-        p.id = row.assetid
-        p.name = row.assetname
-        p.path = row.assetpath
-        p.status = row.assetstatus
-        results.append(p)
-    return results
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
 
-def update(assetid, key, status=''):
-    # Update
-    asset = session.query(Assets).filter(Assets.assetid==assetid).first()
-    if asset:
-        value = asset.assetidentificationkey
-        if value:
-            asset.assetidentificationkey = '%s,%s'%(value, key)
-        else:
-            asset.assetidentificationkey = key
-        if status:
-            asset.assetstatus = status
-        session.commit()
-    
-def failure(assetid, status='Failure'):
-    # Update
-    asset = session.query(Assets).filter(Assets.assetid==assetid).first()
-    asset.assetstatus = status
-    session.commit()
-    
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
 if __name__ == '__main__':
-    print(get_assets())
+  # construct the argument parser and parse the arguments
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-p", "--path", required=True, help="path to input video")
+  ap.add_argument("-e", "--enable_imshow", required=False, help="Enable Imshow")
+  ap.add_argument("-i", "--object_of_interest", required=False, help="Object of interest")
+  args = vars(ap.parse_args())
+  path = args["path"]
+  enable_imshow = int(args.get("enable_imshow", '0'))
+  ooi = args["object_of_interest"]
+  #path = r'C:\Users\govindareddy\Downloads\VID_20180612_165108.mp4'
+  cap = cv2.VideoCapture(r'%s' % (path))
+
+  with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+      while True:
+        ret, image_np = cap.read()
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        # Actual detection.
+        (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        # Visualization of the results of a detection.
+        result = vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            enable_imshow=enable_imshow
+        )
+        if enable_imshow:
+          from matplotlib import pyplot as plt
+          plt.figure(figsize=IMAGE_SIZE)
+          plt.imshow(image_np)        
+          cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+          if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        else:
+          if type(result) is str:
+            print(result)
+            break
